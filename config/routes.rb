bb021b0281c75a42c12efd21f14cdfa06d94f692
jonaszko <konieczny.jonasz@gmail.com>
2019-01-13 12:41:13 +0100

    Add polymorphic association between comments and other models

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
 
   resources :articles do
     resources :comments, only: [:create, :edit, :update, :destroy]
-    resources :pictures
+    resources :pictures do
+      resources :comments, only: [:create, :edit, :update, :destroy]
+    end
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20190113103045_add_commentable_to_comments.rb
@@ -0,0 +1,15 @@
+class AddCommentableToComments < ActiveRecord::Migration[5.2]
+  def change
+    #Usuwamy ograniczenie klucza obcego dla kolumny article_id w tabeli comments.
+    #Dzięki temu po zmianie nazwy kolumna ta będzie mogła odnosić się do czegoś innego,
+    #niż artykuły
+    remove_foreign_key :comments, :articles
+
+    #Od teraz article_id niech nazywa się commentable_id
+    rename_column :comments, :article_id, :commentable_id
+    #Dodajmy kolumnę commentable_type i ustalmy, że domyślnie polimorficzny
+    add_column :comments, :commentable_type, :string, null: false, default: 'Article'
+
+    add_index :comments, [:commentable_type, :commentable_id]
+  end
+end
